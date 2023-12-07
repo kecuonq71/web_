3182968fc32e0d15bf7b1d1a4216813b82b7d76d
Minh Khang <94234570+khanglag@users.noreply.github.com>
2023-12-07 21:47:48 +0700

    uo

--- admin1/js/adminedit.js
+++ admin1/js/adminedit.js
@@ -35,16 +35,17 @@ function editAdmin(idEdit){
     
     var printEdit='';
     var index ,id,brand;
-    
+
     for(i=0;i<products.length;i++){
-        if(products[i].productId==idEdit){
+        if(products[i].productName==idEdit){
+          
             var a=`
                     <div class="edit-img">
-                        <img src=".${products[i].img}">
+                        <img id="productImage" src=".${products[i].image}">
                     </div>  
                     <div class="item-body item-body-ae">
                     <p class="text">Tên sản phẩm</p>
-                    <input type="text" id="namesp" value="${products[i].name}"> 
+                    <input type="text" id="namesp" value="${products[i].productName}"> 
                     <div class="container-input-error errorDK5">
                         <p class="error">
                             Vui lòng tên sản phẩm
@@ -59,93 +60,36 @@ function editAdmin(idEdit){
                                 Vui lòng nhập giá
                         </div>
                     </div>  
-                    <div class="item-body item-body-ae">
-                        <p class="text">Nhập đường đẫn ảnh</p>
-                        <input type="file" id="myFileEdit" name="filename">
-                        
-                        <div class="container-input-error errorDK7">
-                            <p class="error">
-                                Vui lòng đường đẫn ảnh
-                            </p>
-                        </div>
-                    </div>`;
+                    `;
                     printEdit+=a;
                     index=i;
-                    id = products[i].productId;
+                    id = i;
                     brand = products[i].brand;
                     break;
         }
     }
-    var update = `<button class="btn-confirm " onclick="updateInfo(`+index+`,`+id+`,\``+brand+`\`);">Cập nhật</button>`;
+    var update = `<button class="btn-confirm " onclick="updateInfo('${id}','${products[i].image}');">Cập nhật</button>`;
     document.getElementById("edit").innerHTML = printEdit;
     document.querySelector(".btn-confirm-ae").innerHTML =update;
 }
-// function editAdmin(idEdit) {
-//     var products = localStorage.getItem('products') ? JSON.parse(localStorage.getItem('products')) : [];
-//     document.querySelector(".modal-ae-edit").style.display = 'flex';
-
-//     var printEdit = '';
-//     var index, id, brand;
-
-//     for (i = 0; i < products.length; i++) {
-//         if (i == idEdit) {  // Thay đổi đây
-//            var a=`
-//                     <div class="edit-img">
-//                         <img src=".${products[i].img}">
-//                     </div>  
-//                     <div class="item-body item-body-ae">
-//                     <p class="text">Tên sản phẩm</p>
-//                     <input type="text" id="namesp" value="${products[i].name}"> 
-//                     <div class="container-input-error errorDK5">
-//                         <p class="error">
-//                             Vui lòng tên sản phẩm
-//                         </p>
-//                     </div>  
-//                      </div>
-//                     <div class="item-body item-body-ae">
-//                         <p class="text">Nhập giá</p>
-//                         <input type="text" id="giasp" value="${products[i].price}">   
-//                         <div class="container-input-error errorDK6">
-//                             <p class="error">
-//                                 Vui lòng nhập giá
-//                         </div>
-//                     </div>  
-//                     <div class="item-body item-body-ae">
-//                         <p class="text">Nhập đường đẫn ảnh</p>
-//                         <input type="file" id="myFileEdit" value = "${products[i].im}" >
-                        
-//                         <div class="container-input-error errorDK7">
-//                             <p class="error">
-//                                 Vui lòng đường đẫn ảnh
-//                             </p>
-//                         </div>
-//                     </div>`;
-//             printEdit += a;
-//             index = i;
-//             id = products[i].productId;
-//             brand = products[i].brand;
-//             break;
-//         }
-//     }
-
-//     var update = `<button class="btn-confirm " onclick="updateInfo(${index},${id},\`${brand}\`);">Cập nhật</button>`;
-//     document.getElementById("edit").innerHTML = printEdit;
-//     document.querySelector(".btn-confirm-ae").innerHTML = update;
-// }
-
 
 // cập nhật
-function updateInfo(index,id,brand){
+function updateInfo(productId,img){
+    var linkImg = img;
+    var productName = document.getElementById('namesp').value;
+    var productPrice = +document.getElementById('giasp').value;
+    console.log("Ảnh sp: -> "+linkImg + "  tên sp: "+productName+ "  giá sp: "+productPrice)
     var products = localStorage.getItem('products') ? JSON.parse(localStorage.getItem('products')) : [];
-    var img;
-    if(document.getElementById('myFileEdit').value==null){
-        img = id+'.jpg';
-    }else{
-        img = document.getElementById('myFileEdit').value.match(/[\/\\]([\w\d\s\.\-\(\)]+)$/)[1]
+    for (var i = 0; i < products.length; i++) {
+        if(i == productId){
+            products[i].productName = productName;
+            products[i].price = productPrice;
+            break;
+        }
+        
     }
-    products[index]={productId: id, brand: brand,img: "./assets/img/products/"+img+"" ,name: document.getElementById("namesp").value,price:document.getElementById("giasp").value};
-    localStorage.setItem('products',JSON.stringify(products)); 
-    renderListEdit(1);
+    localStorage.setItem('products', JSON.stringify(products));
+    showAE();
     closeEdit();
 }
 
